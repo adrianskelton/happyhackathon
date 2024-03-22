0c3cd2e83684e1c4807adab4bef67ac89d55e9eb
Adrian Skelton <adrianskelton9@gmail.com>
2024-03-22 11:41:38 +0000

    change to models

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    favorite_affirmations = models.ManyToManyField('Affirmation', blank=True)
+    favorite_affirmations = models.ManyToManyField('affirmation', blank=True)
     EMOTION_CHOICES = [
         ('happy', '😊'),
         ('sad', '😢'),
