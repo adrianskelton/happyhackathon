366b9a5bc362fd69ba2637beb9b4fcaa6531e91e
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-03-23 22:48:37 +0000

    added rating modal

--- positive/urls.py
+++ positive/urls.py
@@ -30,5 +30,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
+    path('', home, name='home'),
+    
 ]
 
