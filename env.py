72e0704b0bdbb7d63dc1438e1bdb1ee545da102c
Adrian Skelton <adrianskelton9@gmail.com>
2024-03-23 15:40:56 +0000

    add settings.py

--- env.py
+++ env.py
@@ -2,18 +2,18 @@ import os
 import dj_database_url
 
 # Function to load environment variables from the .env file
-#def load_env():
-#    try:
-#        with open('.env', 'r') as f:
-#            for line in f:
-#                if line.strip() and not line.startswith('#'):
-#                    key, value = line.strip().split('=', 1)
-#                    os.environ.setdefault(key, value)
-#    except FileNotFoundError:
-#        print("No .env file found. Environment variables are not loaded.")
+def load_env():
+    try:
+        with open('.env', 'r') as f:
+            for line in f:
+                if line.strip() and not line.startswith('#'):
+                    key, value = line.strip().split('=', 1)
+                    os.environ.setdefault(key, value)
+    except FileNotFoundError:
+        print("No .env file found. Environment variables are not loaded.")
 
 # Load environment variables
-#load_env()
+load_env()
 
 # Database settings
 DATABASE_URL = os.environ.get('DATABASE_URL')
