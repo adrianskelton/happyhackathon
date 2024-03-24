647b6afcde86c0c7ef9565ad9a901dbe0e24bb0c
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-03-24 10:40:48 +0000

    Set up Adriannas merge conflict

--- affirmation/models.py
+++ affirmation/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
+# Model representing a category for blog posts
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=200, unique=True, null=False)
+    icon = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
 
 class Affirmation(models.Model):
     text = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='affirmations', default=None)
 
     def __str__(self):
         return self.text
