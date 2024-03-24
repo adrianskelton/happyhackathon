28dde56c95ae038f9ce8f7ba362d93450dcb7275
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-03-24 13:54:41 +0000

    Set up the affirmation forms to see the random affirmations

--- affirmation/urls.py
+++ affirmation/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import affirmation
+from .views import affirmation, get_random_affirmation
 
 urlpatterns = [
     # URL pattern for random affirmation without category
     path('affirmation/', affirmation, name='affirmation'),
     # URL pattern for affirmation with category
     path('affirmation/<slug:category_slug>/', affirmation, name='affirmation_by_category'),
+    path('get_random_affirmation/', get_random_affirmation, name='get_random_affirmation'),
 ]
--- affirmation/views.py
+++ affirmation/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category, Affirmation
 from django.views.generic import ListView
+import random
 
 
 def home(request):
@@ -32,3 +33,22 @@ def affirmation(request, category_slug=None):
     # Select a random affirmation from the filtered queryset
     random_affirmation = random.choice(affirmations)
     return render(request, 'affirmation.html', {'affirmation': random_affirmation})
+
+def get_random_affirmation(request):
+    if request.method == 'POST':
+        # Handle POST request to get a random affirmation
+        category_name = request.POST.get('category')
+        try:
+            category = Category.objects.get(title=category_name)
+            affirmations = category.affirmations.all()
+            if affirmations:
+                random_affirmation = random.choice(affirmations)
+                return render(request, 'note.html', {'affirmation': random_affirmation})
+            else:
+                return render(request, 'note.html', {'affirmation': None})
+        except Category.DoesNotExist:
+            return HttpResponse("Category does not exist")
+    else:
+        # Handle GET request to render the form with categories
+        categories = Category.objects.all()
+        return render(request, 'select_category.html', {'categories': categories})
