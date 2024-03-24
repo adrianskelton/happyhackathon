6811d04b01985811b885c759b066448e9b001fc5
Adrian Skelton <adrianskelton9@gmail.com>
2024-03-24 13:31:16 +0000

    add favorites modal and change some url patters in affirmation

--- affirmation/admin.py
+++ affirmation/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Affirmation, Category
+from favorites.models import Favorite
 
 class AffirmationAdmin(admin.ModelAdmin):
     list_display = ('text', 'category', 'created_at')  # Display the 'text' field in the admin list view
 
 admin.site.register(Affirmation, AffirmationAdmin)
 admin.site.register(Category) 
+admin.site.register(Favorite)
--- affirmation/urls.py
+++ affirmation/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import affirmation
+from favorites.views import add_to_favorites
 
 urlpatterns = [
     # URL pattern for random affirmation without category
     path('affirmation/', affirmation, name='affirmation'),
     # URL pattern for affirmation with category
     path('affirmation/<slug:category_slug>/', affirmation, name='affirmation_by_category'),
+    # add to favorites
+    path('favorite/<int:affirmation_id>/', add_to_favorites, name='favorite'),
 ]
